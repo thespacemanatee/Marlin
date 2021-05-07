710096f9e2b3a7aac71327d5c558852aa372d0d9
Chee Kit <cheekit.chong98@gmail.com>
2021-05-07 23:56:22 +0800

    Update PID values for hotend and bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -492,9 +492,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp   22.6
-    #define DEFAULT_Ki   2.71
-    #define DEFAULT_Kd   47.1
+    #define DEFAULT_Kp   14.7
+    #define DEFAULT_Ki   1.26
+    #define DEFAULT_Kd   42.7
   #endif
 #endif // PIDTEMP
 
@@ -533,9 +533,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp  38.68
-  #define DEFAULT_bedKi   7.31
-  #define DEFAULT_bedKd 136.43
+  #define DEFAULT_bedKp  73.6
+  #define DEFAULT_bedKi   9.98
+  #define DEFAULT_bedKd 362.1
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
